5516d1f4930edf722a859cc578b69064d241e965
Sidd <35938702+siddsp02@users.noreply.github.com>
2021-08-24 16:59:35 -0700

    fix type hinting for async_requests.py

--- async_requests.py
+++ async_requests.py
@@ -13,12 +13,8 @@ async def response() -> str:
     return "packet example abc"
 
 
-async def request_packet() -> str:
-    """Simulates a request for some data that is I/O bound.
-
-    Returns:
-        A confirmation that the data has been received.
-    """
+async def request_packet() -> None:
+    """Simulates a request for some data that is I/O bound."""
 
     packet = await response()
     print(packet)
